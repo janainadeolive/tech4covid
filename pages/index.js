3569b539c2e43527de3da7ca263c0f940cafe798
victornns <victor.nascimento.ns@gmail.com>
2020-03-26 23:02:14 -0300

    section header

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Features from '../sections/features'
 import Projects from '../sections/projects'
 import HelpUs from '../sections/help-us'
+import Header from '../sections/header'
 
 export default () => (
   <div className="app">
+    <Header />
     <HelpUs />
     <Features />
-    <Projects />
+    {/* <Projects /> */}
   </div>
 )
---
+++ sections/header.js
@@ -0,0 +1,25 @@
+import styles from '../styles/header.module.scss'
+import Button from '../components/button'
+
+export default () => {
+
+    return (
+        <section className={styles.header}>
+            <div className={styles.wrapper}>
+                <div className='row between-md middle-xs'>
+
+                    <div className='col-xs-12 col-sm-6'>
+                        <a className={styles.logo} href='/'>
+                            <img src="https://ucarecdn.com/e2cfb782-1524-496a-a48f-f97b75440d56/" alt="Logo" />
+                        </a>
+                    </div>
+
+                    <div className={`col-xs-12 col-sm-6 ${styles.right}`}>
+                        <Button name='Junte-se a nós' href='/' />
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    )
+}
