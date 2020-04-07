f9282dc8bf95e9a030ecaf665a5f70acc2d190c9
victornns <victor.nascimento.ns@gmail.com>
2020-04-06 23:40:16 -0300

    footer

--- sections/footer.js
+++ sections/footer.js
@@ -3,12 +3,20 @@ import styles from '../styles/footer.module.scss'
 export default () => {
     return (
         <section className={styles.footer}>
-            <div className={styles.wrapper}>
-                <div className='col-xs-12 col-sm-6'>
-                    © 2020 Tech4Covid19 · A comunidade tecnológica combate a Covid19.
-                </div>
-
-                <div className={`col-xs-12 col-sm-6 ${styles.right}`}>
+            <div className={`${styles.wrapper} container-fluid`}>
+                <div class="row">
+                    <div className='col-xs-12 col-sm-6'>
+                        <p>Realizado por:</p>
+                        <a className={`${styles.logo} ${styles.atados}`} href="https://www.atados.com.br/" target="_blank">
+                            <img src="/logo-atados.svg" alt="Atados" />
+                        </a>
+                    </div>
+                    <div className='col-xs-12 col-sm-6'>
+                        <p>Idealizado por:</p>
+                        <a className={styles.logo} href="https://tech4covid19.org/" target="_blank">
+                            <img src="/tech4covid19.png" alt="Tech4covid19" target="_blank" />
+                        </a>
+                    </div>
                 </div>
             </div>
         </section>
